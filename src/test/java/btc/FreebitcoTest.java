ae9c08f7cbf3213d1bb9f396ad6901a2b7e71153
Fiodar <f.motin@sam-solutions.com>
2018-11-10 22:34:34 +0300

    add verify

--- src/test/java/btc/FreebitcoTest.java
+++ src/test/java/btc/FreebitcoTest.java
@@ -3,6 +3,10 @@ package btc;
 import krans.freebitco.FreebitcoServiceImpl;
 import org.testng.annotations.Test;
 
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+
 public class FreebitcoTest  {
 
     private FreebitcoServiceImpl freebitcoService;
@@ -12,6 +16,7 @@ public class FreebitcoTest  {
         freebitcoService = new FreebitcoServiceImpl();
         freebitcoService.changeIp();
         freebitcoService.login("1FvPJV1C7KsqYVYytxGPbSvrDdkXtaRpyW", "W7cx5U5n6LQ1BlsE");
+        $(byText("REQUIREMENTS TO UNLOCK BONUSES")).shouldBe(visible);
         freebitcoService.getBTC();
     }
 
--- src/test/java/btc/FreebitcoTest2.java
+++ src/test/java/btc/FreebitcoTest2.java
@@ -1,7 +1,12 @@
 package btc;
 
+import com.codeborne.selenide.Condition;
 import krans.freebitco.FreebitcoServiceImpl;
 import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
 
 public class FreebitcoTest2 {
 
@@ -12,6 +17,7 @@ public class FreebitcoTest2 {
         freebitcoService = new FreebitcoServiceImpl();
         freebitcoService.changeIp();
         freebitcoService.login("1HCStu8kdmFD8FifGgxwthP5L7J3zBMTfX", "W7cx5U5n6LQ1BlsE");
+        $(byText("REQUIREMENTS TO UNLOCK BONUSES")).shouldBe(visible);
         freebitcoService.getBTC();
     }
 
